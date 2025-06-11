8598fee738660af7714b3b9f30add1df733fd0cc
alex-Sandro-alves-de-lima <lexjustin5@gmail.com>
2025-06-11 16:42:34 -0300

    mdo

--- src/App.tsx
+++ src/App.tsx
@@ -1,157 +1,41 @@
-import {
-  query,
-  collection,
-  onSnapshot,
-  addDoc,
-  doc,
-  deleteDoc,
-  setDoc,
-  updateDoc,
-} from "firebase/firestore";
-import { useEffect, useState } from "react";
-import { db } from "../firebase";
-import LeitorPage from "./pages/LeitorPage";
-
-interface WebBaseData {
-  name: string;
-  id: string;
-}
-
+import { Layout } from "antd";
+import OnePage from "./pages/OnePage";
+import HeaderChildren from "./components/Header";
+const { Content, Header, Footer } = Layout;
 const App = () => {
-  const [nomes, setNomes] = useState<WebBaseData[]>([]);
-  const [inputName, setInputName] = useState<string>("");
-
-  function handleForm(values: string) {
-    setInputName(values);
-    adicionarNome(inputName);
-    setInputName("");
-  }
-
-
-    function handleFormSubst(id: string,values: string) {
-
-    // setInputName(values);
-    // substituirNome(id,inputName);
-    // setInputName("");
-
-    return <>
-    
-      <div>sadasjk</div>
-    </>
-  }
-
-
-
-  useEffect(() => {
-    /// Busca online todas as moficações da collection
-    const q = query(collection(db, "webBase"));
-    const unsubscribe = onSnapshot(q, (querySnapshot) => {
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      //@ts-expect-error
-      const dados: WebBaseData[] = querySnapshot.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data(),
-      }));
-      setNomes(dados);
-    });
-
-    return () => unsubscribe();
-  }, []);
-
-
-  const adicionarNome = async (value: string) => {
-    try {
-      await addDoc(collection(db, "webBase"), {
-        name: value,
-      });
-    } catch (e) {
-      console.error("Erro ao adicionar documento:", e);
-    }
-  };
-
-  const removerNome = async (id: string) => {
-    try {
-      await deleteDoc(doc(db, "webBase", id));
-      console.log("Nome removido com sucesso!");
-    } catch (e) {
-      console.error("Erro ao remover nome:", e);
-    }
-  };
-
-  // Substitui o documento com um novo conteúdo
-  const substituirNome = async (id: string, novoNome: string) => {
-    try {
-      await setDoc(doc(db, "webBase", id), {
-        name: novoNome,
-        catalogo: 1,
-      });
-      console.log("Documento substituído com sucesso!");
-    } catch (e) {
-      console.error("Erro ao substituir documento:", e);
-    }
-  };
-
-  // Substitui o documento com um novo conteúdo
-  const substituirTudo = async (id: string, novoNome: string) => {
-    try {
-      await updateDoc(doc(db, "webBase", id), {
-        name: novoNome,
-        catalogo: 1,
-      });
-      console.log("Documento substituído com sucesso!");
-    } catch (e) {
-      console.error("Erro ao substituir documento:", e);
-    }
-  };
-
-  // useEffect(() => {
-  // const q = query(collection(db, "webBase"));
-  //  onSnapshot(q, (querySnapshot) => {
-  //   return setNomes(
-  //      querySnapshot.docs.map(
-  //        doc => ( doc.data() as WebBaseData)
-  //      ));
-  // });
-
-  // }, []);
-
+  // const {
+  //   token: { colorBgContainer },
+  // } = theme.useToken();
   return (
-    <div style={{ margin: 50 }}>
-      <LeitorPage/>
-      <h1>Nomes</h1>
-      <h4>Adiconar nome:</h4>
-      <input
-        type="text"
-        title="Adicionar nome:"
-        value={inputName}
-        onChange={(e) => setInputName(e.target.value)}
-      />
-      <button onClick={() => handleForm(inputName)}>Adicionar Nome</button>
-      <br />
-      <h4>Lista de nomes cadastrados</h4>
-      {nomes.map((item) => (
-        <div key={item.id}>
-          <div>
-            <input
-              type="text"
-              value={item.name}
-              onChange={(e) => setInputName(e.target.value)}
-            />
-          </div>
+    <>
+      <Layout style={{ minHeight: "100vh" }}>
+        <Header
+          style={{
+            position: "sticky",
+            top: 0,
+            zIndex: 1,
+            width: "100%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+            background: "#fff",
+            boxShadow: "0 2px 8px #f0f1f2", //rgb(45, 68, 90)
+            padding: "0 20px",
+          }}
+        >
+          <HeaderChildren />
+        </Header>
 
-          <div style={{ marginBottom: 15, initialLetter: 10 }}>
-            <button onClick={() => removerNome(item.id)}>Remover</button>
-            <button onClick={() =>  handleFormSubst(item.id,inputName)}>
-              Substituir
-            </button>
-            <button onClick={() => substituirTudo(item.id, "Atualizado")}>
-              Substituir tudo
-            </button>
-          </div>
-        </div>
-      ))}
-    </div>
+        <Content style={{ padding: "40px 20px", background: 'linear-gradient(to right, rgba(42, 157, 143, 0.08), rgba(11, 65, 92, 0.08))'}}>
+          <OnePage />
+        </Content>
+        <Footer style={{ textAlign: "center" }}>
+          Full Automate Web Solutions ©2025 — É sempre possível melhorar
+        </Footer>
+      </Layout>
+    </>
   );
+  // </ConfigProvider>
 };
 
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,48 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//   // host: true,
+//   port: 5173,
+// }
+
+// })
+
+// ///ngrok http 5173
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
+
 export default defineConfig({
-  plugins: [react()],
-  server: {
-  // host: true,
-  port: 5173,
-}
-
-})
-
-///ngrok http 5173
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Meu App React',
+        short_name: 'MeuApp',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#3498db',
+        icons: [
+          {
+            src: 'icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+});
